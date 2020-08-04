52a838c4f8f94db8c8548ad9b4a5f2089bf14cf7
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2020-08-04 12:00:31 +0200

    refactoring

--- Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
+++ Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
@@ -26,7 +26,8 @@ namespace Azunyuuuuuuu.DivaToQuaverConverter
         [CommandOption("destination", 'd', Description = "Path to where the converted files will be written to.")]
         public string DestinationPath { get; set; } = "output";
 
-        private List<string> database = new List<string>();
+        private List<string> _database = new List<string>();
+        private List<Song> _songs = new List<Song>();
 
         public async ValueTask ExecuteAsync(IConsole console)
         {
@@ -34,21 +35,7 @@ namespace Azunyuuuuuuu.DivaToQuaverConverter
 
             await LoadDbFilesIntoMemory();
 
-            // parse pv_* data
-            var pvs = database.Where(x => x.StartsWith("pv_"))
-                .GroupBy(x => x.Split('.').First(), x => KeyValuePair.Create(x.Split('.',2)[1].Split('=').First(), x.Split('=',2).Last()));
-
-            foreach (var pv in pvs)
-            {
-                var temp = new
-                {
-                    Id = pv.Key,
-                    Name = pv.First(x => x.Key == "song_name").Value,
-                    SongFile = pv.First(x => x.Key == "song_file_name").Value,
-                    Artist = pv.First(x => x.Key == "songinfo.music").Value,
-                    Bpm = pv.First(x => x.Key == "bpm").Value,
-                };
-            }
+            ParseDbDataAsSongs();
         }
 
         private async Task LoadDbFilesIntoMemory()
@@ -62,8 +49,32 @@ namespace Azunyuuuuuuu.DivaToQuaverConverter
                     .Where(x => !string.IsNullOrWhiteSpace(x))
                     .Where(x => !x.StartsWith('#'));
 
-                database.AddRange(temp);
+                _database.AddRange(temp);
             }
         }
+
+        private void ParseDbDataAsSongs()
+        {
+            var pvs = _database.Where(x => x.StartsWith("pv_"))
+                .GroupBy(x => x.Split('.').First(), x => KeyValuePair.Create(x.Split('.', 2)[1].Split('=').First(), x.Split('=', 2).Last()));
+
+            _songs.AddRange(pvs.Select(pv => new Song
+            {
+                Id = pv.Key,
+                Name = pv.First(x => x.Key == "song_name").Value,
+                SongFile = pv.First(x => x.Key == "song_file_name").Value,
+                Artist = pv.First(x => x.Key == "songinfo.music").Value,
+                Bpm = pv.First(x => x.Key == "bpm").Value,
+            }));
+        }
+    }
+
+    internal class Song
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string SongFile { get; set; }
+        public string Artist { get; set; }
+        public string Bpm { get; set; }
     }
 }
