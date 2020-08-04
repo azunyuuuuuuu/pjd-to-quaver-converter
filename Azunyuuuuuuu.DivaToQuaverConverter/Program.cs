781fb4caa642a84f82f659d9141dc8d41575f360
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2020-08-04 10:51:05 +0200

    load db files into memory

--- Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
+++ Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
@@ -18,17 +21,33 @@ namespace Azunyuuuuuuu.DivaToQuaverConverter
     public class ConvertCommand : ICommand
     {
         [CommandOption("source", 's', Description = "Path to the source files.")]
-        public string SourcePath { get; set; } = "./input/";
+        public string SourcePath { get; set; } = "input";
 
         [CommandOption("destination", 'd', Description = "Path to where the converted files will be written to.")]
-        public string DestinationPath { get; set; } = "./output/";
+        public string DestinationPath { get; set; } = "output";
 
-        public ValueTask ExecuteAsync(IConsole console)
+        private Dictionary<string, string> database = new Dictionary<string, string>();
+
+        public async ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine("Hello world!");
+            console.Output.WriteLine($"Converting files from '{SourcePath}'");
 
-            // Return empty task because our command executes synchronously
-            return default;
+            await LoadDbFilesIntoMemory();
+        }
+
+        private async Task LoadDbFilesIntoMemory()
+        {
+            var dbfilepaths = Directory.GetFiles(Path.Combine(SourcePath, "rom"), "*.txt");
+
+            foreach (var filepath in dbfilepaths)
+            {
+                var lines = (await File.ReadAllLinesAsync(filepath))
+                    .Where(x => !x.StartsWith('#'))
+                    .Select(x => x.Split('='));
+
+                foreach (var line in lines)
+                    database.TryAdd(line[0], line[1]);
+            }
         }
     }
 }
