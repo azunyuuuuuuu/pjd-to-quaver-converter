891e7e76c62d5250b1320f3658e748d442e9192c
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2020-08-04 09:53:09 +0200

    basic commandline implementation of CliFx

--- Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
+++ Azunyuuuuuuu.DivaToQuaverConverter/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
 
 namespace Azunyuuuuuuu.DivaToQuaverConverter
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+    }
+
+    [Command(Description = "Converts script files from Project Diva to Quaver.")]
+    public class ConvertCommand : ICommand
+    {
+        [CommandOption("source", 's', Description = "Path to the source files.")]
+        public string SourcePath { get; set; } = "./input/";
+
+        [CommandOption("destination", 'd', Description = "Path to where the converted files will be written to.")]
+        public string DestinationPath { get; set; } = "./output/";
+
+        public ValueTask ExecuteAsync(IConsole console)
         {
-            Console.WriteLine("Hello World!");
+            console.Output.WriteLine("Hello world!");
+
+            // Return empty task because our command executes synchronously
+            return default;
         }
     }
 }
