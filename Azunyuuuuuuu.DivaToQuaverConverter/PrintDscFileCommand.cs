e3171c69c09f4873e43e9380efd0a31c16c7df03
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2020-08-09 13:32:03 +0200

    printing out all notes of a .dsc file

---
+++ Azunyuuuuuuu.DivaToQuaverConverter/PrintDscFileCommand.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+
+namespace Azunyuuuuuuu.DivaToQuaverConverter
+{
+    [Command("print dsc", Description = "Prints out a .dsc file in human readable form.")]
+    public class PrintDscFileCommand : ICommand
+    {
+        [CommandParameter(0, Description = "Input file")]
+        public string InputFile { get; set; }
+
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            var dsc = DscFile.LoadFile(InputFile);
+
+            foreach (var note in dsc.GetAllNotes())
+                console.Output.WriteLine($"{note.Timestamp}, {note.Button}, {note.TargetPosX}, {note.TargetPosY}, {note.StartPosX}, {note.StartPosY}, {note.Unknown6}, {note.Unknown7}");
+
+            return default;
+        }
+    }
+}
